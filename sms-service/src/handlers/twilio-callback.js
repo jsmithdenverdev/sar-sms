ba765963d6818573a0cccea9a3ec35c932db8bc8
Jacob Smith <jsmithdenverdev@gmail.com>
2019-05-02 18:00:14 -0600

    Added actual parsing, etc to twilio-callback

--- sms-service/src/handlers/twilio-callback.js
+++ sms-service/src/handlers/twilio-callback.js
@@ -1,6 +1,19 @@
+const qs = require("querystring");
+const { events, emitter, wireEvents } = require('../events');
+const handleError = require('../events/subscribers/handle-error');
+const sendSmsCallback = require('../events/subscribers/send-sms-callback');
+const publishInboundSmsToSns = require('../events/subscribers/publish-inbound-sms-to-sns');
+
 module.exports.handle = (event, context, callback) => {
-  callback(null, {
-    body: JSON.stringify({ name: "twilio-callback" }),
-    statusCode: 200
-  });
+  const parsed = qs.parse(event.body);
+
+  const errorHandler = handleError(callback);
+
+  wireEvents({
+    [events.SMS_RECIEVED]: publishInboundSmsToSns,
+    [events.PROCESSING_COMPLETED]: sendSmsCallback(callback),
+    [events.ERROR]: errorHandler
+  })
+  
+  emitter.emit(events.SMS_RECIEVED, parsed)
 };
