a9336ea2d2e86f70cc549a60202cde887c12ef14
Jacob Smith <jsmithdenverdev@gmail.com>
2019-05-10 17:50:29 -0600

    Moved wireEvents into its own file

--- sms-service/src/events/index.js
+++ sms-service/src/events/index.js
@@ -1,17 +1,9 @@
 const emitter = require("./emitter");
 const events = require("./events");
+const wireEvents = require("./wire-events");
 
 module.exports = {
   emitter,
   events,
-  wireEvents(events, removeExisting = true) {
-    // To ensure we don't have duplicate events defined
-    if (removeExisting) {
-      emitter.removeAllListeners();
-    }
-
-    Object.keys(events).forEach(event => {
-      emitter.on(event, events[event]);
-    });
-  }
+  wireEvents
 };
