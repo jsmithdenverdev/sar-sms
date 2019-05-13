4d97105169c9f8d4ed8143cd9ec19c6ee7d5c83f
Jacob Smith <jsmithdenverdev@gmail.com>
2019-05-13 16:20:12 -0600

    Fixed creating new message

--- conversation-service/src/events/handlers/message/onCreateMessage.js
+++ conversation-service/src/events/handlers/message/onCreateMessage.js
@@ -1,5 +1,6 @@
 const emitter = require("../../emitter");
-const events = require("../../events");
+const { ERROR, MESSAGE_CREATED } = require("../../events");
+const { MESSAGE_STATUSES } = require("../../../constants/conversation");
 const DynamoDb = require("../../../lib/DynamoDb");
 
 module.exports = async ({ recipient, message }) => {
@@ -17,7 +18,7 @@ module.exports = async ({ recipient, message }) => {
     const conversation = await DynamoDb.read(id);
 
     if (!conversation) {
-      emitter.emit(events.ERROR, {
+      emitter.emit(ERROR, {
         error: "A conversation for this number was not found!"
       });
 
@@ -25,19 +26,27 @@ module.exports = async ({ recipient, message }) => {
     }
 
     const existingMessages = conversation.messages || [];
+    const newMessage = {
+      message,
+      // Once we confirm delivery from Twilio this will be updated to MESSAGES_STATUSES.DELIVERED
+      status: MESSAGE_STATUSES.PENDING
+    };
 
     await DynamoDb.update(
       // Using the recipient phone number as the primary key (ie +1-123-456-7890 => { id: 1234567890 })
       id,
       "set messages = :m",
       {
-        ":m": [...existingMessages, { message }]
+        ":m": [
+          ...existingMessages,
+          newMessage
+        ]
       }
     );
 
-    emitter.emit(events.MESSAGE_CREATED, { recipient, message });
+    emitter.emit(MESSAGE_CREATED, { recipient, message });
   } catch (e) {
-    emitter.emit(events.ERROR, {
+    emitter.emit(ERROR, {
       error: e
     });
   }
