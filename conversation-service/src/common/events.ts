7404e7eaf89457de59519ccdaecc947783737a50
Jacob Smith <jsmithdenverdev@gmail.com>
2019-06-14 16:19:37 -0600

    Moved events to common and changed extension type to .ts.

---
+++ conversation-service/src/common/events.ts
@@ -0,0 +1,24 @@
+// Errors
+// ------------------------
+export const ERROR = "ERROR";
+
+// Conversation
+// ------------------------
+// CREATE
+export const CREATE_CONVERSATION = "CREATE_CONVERSATION";
+export const CONVERSATION_CREATED = "CONVERSATION_CREATED ";
+// READ
+export const READ_CONVERSATION = "READ_CONVERSATION";
+// UPDATE
+export const CONVERSATION_UPDATED = "CONVERSATION_UPDATED";
+// DELETE
+export const DELETE_CONVERSATION = "DELETE_CONVERSATION";
+export const CONVERSATION_DELETED = "CONVERSATION_DELETED ";
+// LIST
+export const LIST_CONVERSATION = "LIST_CONVERSATION";
+
+// Sms
+// ------------------------
+export const CREATE_SMS = "CREATE_SMS";
+export const SMS_CREATED = "SMS_CREATED";
+export const UPDATE_SMS_STATUS = "UPDATE_SMS_STATUS";
