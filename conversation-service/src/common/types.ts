a4aeaa7746c37adce18658509b390e6538b3220f
Jacob Smith <jsmithdenverdev@gmail.com>
2019-06-14 16:22:15 -0600

    Changed EventBinding from a class to a Type

--- conversation-service/src/common/types.ts
+++ conversation-service/src/common/types.ts
@@ -2,6 +2,7 @@ export interface EventHandler {
   handle(payload: any): void;
 }
 
-export class EventBinding {
-  constructor(public name: string, public handler: EventHandler) {}
-}
+export type EventBinding = {
+  name: string;
+  handler: EventHandler;
+};
