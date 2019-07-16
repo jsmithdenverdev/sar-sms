a1d7ae07424f035194ad79d533db8d4cbb469638
Jacob Smith <jsmithdenverdev@gmail.com>
2019-07-16 17:09:19 -0600

    Got rid of events, wire-events, and events barrel

--- conversation-service/src/events/index.js
+++
@@ -1,9 +0,0 @@
-const emitter = require("./emitter");
-const events = require("./events");
-const wireEvents = require("./wire-events");
-
-module.exports = {
-  emitter,
-  events,
-  wireEvents
-};
