c1f655a8650d292eab916ae006482fd3c2afd092
Jacob Smith <jsmithdenverdev@gmail.com>
2019-07-17 17:19:10 -0600

    Wired up onDeleteConversation, onConversationDeleted, and onError.

--- conversation-service/src/lambda/delete-conversation.js
+++ conversation-service/src/lambda/delete-conversation.js
@@ -3,14 +3,18 @@ const events = require("@constants/events");
 const onDeleteConversation = require("@handlers/conversation/onDeleteConversation");
 const onConversationDeleted = require("@handlers/conversation/onConversationDeleted");
 const onError = require("@handlers/error/onError");
+const { deleteConversation } = require("@lib/conversation");
 
 module.exports.handle = (event, _context, callback) => {
   const { pathParameters } = event;
   const { phone } = pathParameters;
 
-  emitter.on(events.DELETE_CONVERSATION, onDeleteConversation);
-  emitter.on(events.CONVERSATION_DELETED, onConversationDeleted);
-  emitter.on(events.ERROR, onError);
+  emitter.on(
+    events.DELETE_CONVERSATION,
+    onDeleteConversation({ emitter, deleteConversation })
+  );
+  emitter.on(events.CONVERSATION_DELETED, onConversationDeleted({ callback }));
+  emitter.on(events.ERROR, onError({ callback }));
 
   const payload = {
     recipient: phone
