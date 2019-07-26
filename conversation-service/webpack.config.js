8513c3f81a8c7d8da8db2092461feda42b46d0d8
Jacob Smith <jsmithdenverdev@gmail.com>
2019-07-25 18:06:27 -0600

    Updated serverless yaml and webpack config

--- conversation-service/webpack.config.js
+++ conversation-service/webpack.config.js
@@ -4,12 +4,10 @@ const nodeExternals = require("webpack-node-externals");
 module.exports = {
   entry: {
     createConversation: "./src/lambda/create-conversation.js",
-    createRecievedSms: "./src/lambda/create-recieved-sms.js",
-    createSms: "./src/lambda/create-sms.js",
+    readConversation: "./src/lambda/read-conversation.js",
     deleteConversation: "./src/lambda/delete-conversation.js",
     listConversations: "./src/lambda/list-conversations.js",
-    readConversation: "./src/lambda/read-conversation.js",
-    updateSmsStatus: "./src/lambda/update-sms-status.js"
+    addSmsToConversation: "./src/lambda/add-sms-to-conversation.js"
   },
   resolve: {
     extensions: [".js", ".json"],
