e94367003922e0755af5200e2c6282db9c96396a
Jacob Smith <jsmithdenverdev@gmail.com>
2019-08-21 15:05:52 -0600

    Changed the structure of the webpack config to create a single entrypoint based config for every entrypoint

--- conversation-service/webpack.config.js
+++ conversation-service/webpack.config.js
@@ -1,31 +1,35 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-module.exports = {
-  entry: {
-    createConversation: "./src/lambda/create-conversation.js",
-    readConversation: "./src/lambda/read-conversation.js",
-    deleteConversation: "./src/lambda/delete-conversation.js",
-    listConversations: "./src/lambda/list-conversations.js",
-    createNewSms: "./src/lambda/create-new-sms.js",
-    createRecievedSms: "./src/lambda/create-recieved-sms.js"
-  },
+const entryPoints = [
+  { createConversation: "./src/lambda/create-conversation.js" },
+  { readConversation: "./src/lambda/read-conversation.js" },
+  { deleteConversation: "./src/lambda/delete-conversation.js" },
+  { listConversations: "./src/lambda/list-conversations.js" },
+  { createNewSms: "./src/lambda/create-new-sms.js" },
+  { createRecievedSms: "./src/lambda/create-recieved-sms.js" }
+];
+
+const buildAliases = {
+  "@common": path.resolve(__dirname, "src/common"),
+  "@constants": path.resolve(__dirname, "src/constants"),
+  "@handlers": path.resolve(__dirname, "src/handlers"),
+  "@lib": path.resolve(__dirname, "src/lib")
+};
+
+const buildConfigs = entryPoints.map(entryPoint => ({
+  entry: entryPoint,
   resolve: {
     extensions: [".js", ".json"],
-    alias: {
-      "@common": path.resolve(__dirname, "src/common"),
-      "@constants": path.resolve(__dirname, "src/constants"),
-      "@handlers": path.resolve(__dirname, "src/handlers"),
-      "@lib": path.resolve(__dirname, "src/lib")
-    }
+    alias: buildAliases
   },
   target: "node",
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  externals: [nodeExternals()],
   output: {
-    libraryTarget: "commonjs2",
+    libraryTarget: "commonjs",
     path: path.join(__dirname, "dist"),
     filename: "[name].js"
-  },
-  devtool: process.env.NODE_ENV === "production" ? "" : "source-map"
-};
+  }
+}));
+
+module.exports = buildConfigs;
